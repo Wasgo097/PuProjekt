8dec2440219e4e7421fcd333ab03df50c5ef022e
Wasgo097 <patrykpakula321@gmail.com>
2021-12-23 17:13:44 +0100

    init

---
+++ UserApp/DllWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace UserApp
+{
+    class DllWrapper
+    {
+        [DllImport("ImageProcessing.dll")]
+        public static extern void fibonacci_init(ulong a, ulong b);
+        [DllImport("ImageProcessing.dll")]
+        public static extern bool fibonacci_next();
+        [DllImport("ImageProcessing.dll")]
+        public static extern ulong fibonacci_current();
+        [DllImport("ImageProcessing.dll")]
+        public static extern uint fibonacci_index();
+    }
+}
