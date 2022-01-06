3165212add9ad74e9130cb63c56a95012b460434
Kamil Paluch <kamil.paluch98@gmail.com>
2022-01-06 17:05:26 +0100

    initial duo working

--- UserApp/DllWrapper.cs
+++ UserApp/DllWrapper.cs
@@ -8,13 +8,13 @@ namespace UserApp
 {
     class DllWrapper
     {
-        [DllImport("ImageProcessing.dll")]
-        public static extern void fibonacci_init(ulong a, ulong b);
-        [DllImport("ImageProcessing.dll")]
-        public static extern bool fibonacci_next();
-        [DllImport("ImageProcessing.dll")]
-        public static extern ulong fibonacci_current();
-        [DllImport("ImageProcessing.dll")]
-        public static extern uint fibonacci_index();
+        //[DllImport("ImageProcessing.dll")]
+        //public static extern void fibonacci_init(ulong a, ulong b);
+        //[DllImport("ImageProcessing.dll")]
+        //public static extern bool fibonacci_next();
+        //[DllImport("ImageProcessing.dll")]
+        //public static extern ulong fibonacci_current();
+        //[DllImport("ImageProcessing.dll")]
+        //public static extern uint fibonacci_index();
     }
 }
