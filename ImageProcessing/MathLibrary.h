c837305c04c7438a08d64e8b497d228464d29b75
Wasgo097 <patrykpakula321@gmail.com>
2022-01-12 23:36:36 +0100

    Refactor

--- ImageProcessing/MathLibrary.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-extern "C" __declspec(dllexport) void fibonacci_init(unsigned long a, unsigned long b);
-extern "C" __declspec(dllexport) bool fibonacci_next();
-extern "C" __declspec(dllexport) unsigned long fibonacci_current();
-extern "C" __declspec(dllexport) unsigned int fibonacci_index();
